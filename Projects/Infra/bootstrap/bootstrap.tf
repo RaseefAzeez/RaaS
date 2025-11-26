1af0f2e1985ed30b3a1b792199073714e9271f64
RaseefAzeez <raseefazeez@gmail.com>
2025-11-26 19:40:57 +0530

    env-var updated on infra-dev-setup module

--- Projects/Infra/bootstrap/bootstrap.tf
+++ Projects/Infra/bootstrap/bootstrap.tf
@@ -13,10 +13,10 @@ provider "aws" {
 }
 
 module "iam_oidc_provider" {
-  source    = "terraform-aws-modules/iam/aws//modules/iam-oidc-provider"
+  source  = "terraform-aws-modules/iam/aws//modules/iam-oidc-provider"
   version = "~> 6.0"
 
-  url = "https://token.actions.githubusercontent.com"
+  url            = "https://token.actions.githubusercontent.com"
   client_id_list = ["sts.amazonaws.com"]
 
   tags = {
@@ -25,27 +25,27 @@ module "iam_oidc_provider" {
   }
 }
 
-# Module for OIDC role with policy added with (admin access)
+# Module for OIDC role with policy added with (admin access provided)
 module "iam_role_github_oidc" {
-  source    = "terraform-aws-modules/iam/aws//modules/iam-role"
-  
-    role_name = "GitHub-OIDC-RaaS-Deploy-Role"
+  source = "terraform-aws-modules/iam/aws//modules/iam-role"
 
-    trusted_oidc_providers = [
+  role_name = "GitHub-OIDC-RaaS-Deploy-Role"
+
+  trusted_oidc_providers = [
     module.iam_oidc_provider.arn
   ]
-    
+
   # This should be updated to suit your organization, repository, references/branches, etc.
   oidc_subjects = [
     "RaseefAzeez/RaaS:ref:refs/heads/infra-dev-setup",
     "RaseefAzeez/RaaS:ref:refs/heads/main"
   ]
 
-  
+
 
   policies = {
     // S3ReadOnly = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-        TerraformFull = "arn:aws:iam::aws:policy/AdministratorAccess"
+    TerraformFull = "arn:aws:iam::aws:policy/AdministratorAccess"
   }
 
   tags = {
