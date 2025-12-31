8c54874cbeb41b92a18d551fe0cd056b376e61a7
Raseef Azeez(UST <166970@ust.com>
2025-12-31 15:38:18 +0530

    updated secret value format on git

--- Projects/Infra/envs/prod/main.tf
+++ Projects/Infra/envs/prod/main.tf
@@ -12,7 +12,7 @@ module "raas" {
   instance_map = var.instance_map
   group_map = var.group_map
   cognito_user_pool_id = var.cognito_user_pool_id
-  cognito_app_client_id = [var.cognito_app_client_id]
+  cognito_app_client_id = var.cognito_app_client_id
   frontend_origin = var.frontend_origin
 }
 
