12365cdaa91caab4924a5c3b581f7fe50e2f2f56
Raseef Azeez(UST <166970@ust.com>
2025-12-31 15:18:36 +0530

    made the app client id to string

--- Projects/Infra/envs/dev/main.tf
+++ Projects/Infra/envs/dev/main.tf
@@ -11,6 +11,6 @@ module "raas" {
   instance_map = var.instance_map
   group_map = var.group_map
   cognito_user_pool_id = var.cognito_user_pool_id
-  cognito_app_client_id = [var.cognito_app_client_id]
+  cognito_app_client_id = var.cognito_app_client_id
   frontend_origin = var.frontend_origin
 }
